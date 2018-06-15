59ac30e16ef88bcba3a02192f4f321debae5db99
Daniel-Kellberg <40282717+Daniel-Kellberg@users.noreply.github.com>
2018-06-14 21:45:38 -0400

    Add files via upload
    First Submission

---
+++ P2T1_Distance_Traveled_KellbergDaniel.py
@@ -0,0 +1,12 @@
+# CTI 110
+# P2T1-Distance Traveled
+# Daniel Kellberg
+# June 13, 2018
+speed = 70
+print ('After 6 hours, the car will travel ' + str((speed * 6)) )
+
+speed = 70
+print ('After 10 hours, the car will travel ' + str((speed * 10)) )
+
+speed = 70
+print ('After 15 hours, the car wil travel ' + str((speed * 15)) )
